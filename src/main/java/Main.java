6924e9c198ad13b367308a526e2c64bc6d29929d
Coral Estrada <coralestrada28@gmail.com>
2025-12-02 10:27:21 -0600

    Completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,48 +1,109 @@
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-// 1. open a connection to the database
-// use the database URL to point to the correct database
-        Connection connection = null;
+    static Scanner scanner = new Scanner(System.in);
+    static boolean doWhile = true;
+    static int homeScreenSelection = 0;
 
-        try {
-            connection = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/northwind",
-                    "root",
-                    "yearup");
+    public static void main(String[] args){
+        homeScreen();
+    }
 
-// define your query
-            String query = """
-                    select ProductID, ProductName, UnitPrice, UnitsInStock
-                    FROM  Products""";
+    public static void homeScreen(){
 
-// Create statement
-// the statement is tied to the open connection
-            Statement statement = connection.prepareStatement(query);
+        do{
+            System.out.println("""
+                Select an option:
+                1) Display all products
+                2) Display all customers
+                3) Display all categories
+                0) Exit""");
 
-// 2. Execute your query
-            ResultSet results = statement.executeQuery(query);
-// process the results
-            while (results.next()) {
-                String productID = results.getString("ProductID");
-                String productName = results.getString("ProductName");
-                String unitsInStock = results.getString("ProductName");
+            homeScreenSelection = Integer.parseInt(scanner.nextLine());
 
-                System.out.println(productID);
-
-                System.out.println("ProductID: " + productID);
-                System.out.println("Name: " + productName);
-                System.out.println("Units in Stock: " + unitsInStock);
-                System.out.println("---------------\n");
+            if (homeScreenSelection == 0){
+                closeApp();
+            } else if (homeScreenSelection == 1) {
+                displayAllProducts();
+            } else if (homeScreenSelection == 2) {
+                displayAllCustomers();
+            } else if (homeScreenSelection == 3) {
+                displayAllCategories();
+            } else {
+                System.out.println("Try again. PLease select a valid entry.");
+                homeScreenSelection = 0;
             }
-        } catch (SQLException e){
-            throw new RuntimeException(e);
-        }
 
+        } while (doWhile);
+    }
 
-// 3. Close the connection
-        connection.close();
+    public static void closeApp(){
+    System.out.println("All done, bye! :)");
+    scanner.close();
+    }
+
+    public static void displayAllProducts(){
 
     }
-}
+
+    public static void displayAllCategories(){
+        // Making a connection with the database
+    try(Connection connection = DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/northwind",
+            "root",
+            "yearup"))
+    {
+        // the query workspace is inside the () in the prepared statement
+        PreparedStatement preparedStatement = connection.prepareStatement(
+                "Select CategoryID FROM Categories");
+        // Processing query
+        ResultSet results = preparedStatement.executeQuery();
+
+    } catch (Exception e) {
+        throw new RuntimeException(e);
+    }
+    }
+
+    public static void displayAllCustomers() {
+            // 1. open a connection to the database
+            // use the database URL to point to the correct database
+            try (Connection connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/northwind",
+                    "root",
+                    "yearup")) {
+
+
+                // define your query
+                String query = """
+                        select CustomerID, CompanyName
+                        FROM  Customers
+                        """;
+
+                // Create statement
+                // the statement is tied to the open connection
+                PreparedStatement preparedstatement = connection.prepareStatement(query);
+
+                // 2. Execute your query
+                ResultSet results = preparedstatement.executeQuery(query);
+
+                // process the results
+                while (results.next()) {
+                    String customerID = results.getString("CustomerID");
+                    String customerName = results.getString("Customer`Name");
+
+                    System.out.println("Customer ID: " + customerID);
+                    System.out.println("Customer Name: " + customerName);
+                    System.out.println("---------------\n");
+                }
+
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+
+
+
+
